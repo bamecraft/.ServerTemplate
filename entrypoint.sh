6d93c676d68d41f971b4f05056d7717eb297eae9
katabame <ayame@kataba.me>
2023-05-18 21:53:01 +0900

    Improve: [server] entrypoint script 📄

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,50 @@
-#!/usr/bin/bash
+#!/bin/bash
 
-## server specific setup scripts are goes here
-## e.g. Download server file and execute
-echo 'You need edit entrypoint.sh file'
-echo "Current working directory is: $(pwd)"
+# ---- BEGIN OF CONFIG SECTION ---- #
+
+# Tips:
+#   You can split value as variable
+#   e.g.
+#       minecraftVersion='1.19.4'
+#       artifactName=$(curl -fsSL -H 'accept: application/json' 'https://example.com/api/${minecraftVersion}/json' | jq -r '.artifacts[1].fileName')
+#       downloadUrl="https://example.com/download/${artifactName}"
+
+# Mandatory variables
+downloadUrl=""
+allocatedMemorySize=""
+
+# Optional variables
+additionalJvmArguments=""
+additonalScriptPath=""
+
+# ----- END OF CONFIG SECTION ----- #
+
+##### !! DO NOT EDIT BELOW !! #####
+
+echo 'Executing entrypoint...'
+
+# Check necessary variables are defined
+if [ -z ${downloadUrl} ]; then
+    echo 'You must write script as downloadUrl variable to download server jar file at config section in entrypoint.sh' >&2
+    exit 1
+elif [ -z ${allocatedMemorySize} ]; then
+    echo 'You must specify allocated memory size for server as allocatedMemorySize variable at config section in entrypoint.sh' >&2
+    exit 1
+fi
+
+# Make sure we are in correct workdir
+cd /server
+
+# Download server jar artifact
+echo 'Downloading server jar file...'
+curl -fsSL -o /server/server.jar ${downloadUrl}
+
+# Run additonal script if defined
+if [ ! -z ${additonalScriptPath} ]; then
+    echo 'Executing additonal script...'
+    bash -c ${additionalScriptPath}
+fi
+
+# Run server
+echo 'Booting server...'
+java -Xmx${allocatedMemorySize} -Xms${allocatedMemorySize} ${additionalJvmArguments} -jar server.jar --nogui
